43777909bb1aed01fe423cea027d9744e4d8fb96
abalchiumas <a.balchiumas@aluno.ufabc.edu.br>
2022-02-22 11:49:37 -0300

    Programação Estruturada: teste beecrowd 1047 corrigido

--- beecrowd-listas/estruturas-condicionais/beecrowd-1047.py
+++ beecrowd-listas/estruturas-condicionais/beecrowd-1047.py
@@ -1,21 +1,21 @@
 x = str(input())
 xList = x.split()
+d0 = [int(xList[0]),int(xList[1])]
+d1 = [int(xList[2]),int(xList[3])]
 
-n = list()
-for i in range(len(xList)):
-    n.append(int(xList[i]))
-
-total = (((n[2]*60)+n[3])-((n[0]*60)+n[1]))
-
-if (1 <= total <= 24*60):
-    if (total < 60):
-        h = 0
-        m = total
+if (d0[0] == d1[0]):
+    if (d0[1] == d1[1]):
+        h, m = 24, 0
+    elif (d0[1] > d1[1]):
+        h, m = 23, (60 - (d0[1] - d1[1]))
     else:
-        if (n[0] == n[2]) and (n[1] == n[3]):
-            h = 24
-            m = 0
-        else:
-            h = total % 60
-            m = total-60*(n[2]-n[0])
-    print(f"O JOGO DUROU {h:.0f} HORA(S) E {m:.0f} MINUTO(S)")
+        h, m = 0, (d1[1] - d0[1])
+else:
+    if(d0[0] > d1[0]):
+        total = abs((((d1[0] + 24) * 60) + d1[1]) - ((d0[0] * 60) + d0[1]))
+    else:
+        total = abs((((d1[0]) * 60) + d1[1]) - ((d0[0] * 60) + d0[1]))
+    h = total // 60
+    m = total - (h * 60)
+
+print(f"O JOGO DUROU {h:.0f} HORA(S) E {m:.0f} MINUTO(S)")
